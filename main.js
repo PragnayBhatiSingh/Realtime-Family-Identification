df8874dcfc289d80290d61870f0e5ac2b62890dd
PragnayBhatiSingh <83853610+PragnayBhatiSingh@users.noreply.github.com>
2021-06-11 12:47:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
-function setup(){
-    canvas=createCanvas(2550 , 250);
-    canvas.position(500, 90);    
+function setup() {
+    canvas = createCanvas(250, 250);
+    canvas.position(500, 90);
+    video = createCapture(VIDEO);
+    video.hide();
+
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/_ON-rumj0/model.json", modal_loaded);
+}
+
+function modal_loaded() {
+    console.log("Teachable Machine Is Loaded");
+}
+
+function draw() {
+    image(video, 0, 0, 250, 250);
+
+    classifier.classify(video, got_results);
+
+}
+
+function got_results(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("object").innerHTML = results[0].label;
+        document.getElementById("accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
